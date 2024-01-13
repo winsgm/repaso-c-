9ac8a8722f62c92dca95484cc8368ae9f9ab5d2b
winsgm <155786422+winsgm@users.noreply.github.com>
2024-01-12 21:20:25 -0400

    Add files via upload

---
+++ diametro y circunferencia.cpp
@@ -0,0 +1,13 @@
+//de:winston garrido montaño
+#include <iostream>
+using namespace std;
+//#define PI 3.1416; //definimos ña constante llamada PI
+
+int main(int argc, char *argv[]) {
+	const float PI=3.1416; //definimos una constante llamada PI
+	double diametro, circunferencia;
+	circunferencia=20;
+	diametro=circunferencia/PI;
+	cout << "el diametro de la circunferencia=20:"<<diametro; 
+	return 0;
+}
