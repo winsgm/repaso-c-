770f92b960a6563c3e4821349956ad4ecfa36aff
winsgm <155786422+winsgm@users.noreply.github.com>
2024-01-12 21:20:08 -0400

    Add files via upload

---
+++ definicion valor PI.cpp
@@ -0,0 +1,12 @@
+//de:winston garrido montaño
+#include <iostream>
+using namespace std;
+//#define PI 3.1416; //definimos ña constante llamada PI
+
+int main(int argc, char *argv[]) {
+	const float PI=3.1416; //definimos una constante llamada PI
+	double formula=PI+100;
+	cout << "mostrando el valor de PI:"<<formula; 
+	return 0;
+}
+
