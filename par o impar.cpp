b08acf34e842b61b731375dc146b2c95c545d54c
winsgm <155786422+winsgm@users.noreply.github.com>
2024-01-12 21:54:04 -0400

    Add files via upload

---
+++ par o impar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+	int numero;
+	
+	// Solicitar al usuario que ingrese un número
+	std::cout << "Ingrese un número entero: ";
+	std::cin >> numero;
+	
+	// Verificar si el número es par o impar
+	if (numero % 2 == 0) {
+		std::cout << numero << " es un número par." << std::endl;
+	} else {
+		std::cout << numero << " es un número impar." << std::endl;
+	}
+	
+	return 0;
+}
